e9804e3d2336fd41a15f613cdca2d1f6af9347d5
harrisonju1 <harrisonju@gmail.com>
2017-04-22 06:29:46 -0400

    Initial commit

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,17 +1,18 @@
 var React = require('react');
 var Nav = require('Nav');
 
+
 var Main = (props) => {
   return(
     <div>
-      <Nav/>
-      <div className="row">
-        <div className="columns medium-6 large-4 small-centered">
-            {props.children}
+      <div>
+        <div>
+          <Nav/>
+          <p>Main.jsx Rendered</p>
+          {props.children}
         </div>
       </div>
     </div>
-
   );
 };
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,52 +1,33 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-
-
 var Nav = React.createClass({
-  onSearch: function(e){
-    e.preventDefault();
-
-    var location = this.refs.location.value;
-    var encodedLocation = encodeURIComponent(location);
-
-    if (location.length > 0){
-      this.refs.location.value ='';
-      window.location.hash = '#/?location=' + encodedLocation;
-    }
-  },
   render: function(){
-    return(
-      <div className="top-bar">
-        <div className="top-bar-left">
-          <ul className="menu">
-            <li className="menu-text">React Weather App</li>
-            <li>
-              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            </li>
-            <li>
-              <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-            </li>
-            <li>
-              <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="top-bar-right">
-          <form onSubmit={this.onSearch}>
+      return(
+        <div className="top-bar">
+          <div className="top-bar-left">
             <ul className="menu">
-              <li>
-                <input type="search" placeholder="Search weather by City" ref="location"/>
+              <li className="menu-text">
+                React Timer App
               </li>
               <li>
-                <input type ="submit" className="button" value="Get weather"/>
+                <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+              </li>
+              <li>
+                <Link to="/" activeClassName="active-link">Countdown</Link>
               </li>
             </ul>
-          </form>
+          </div>
+          <div className = "top-bar-right">
+            <ul className="menu">
+              <li className="menu-text">
+                Created by <a href="https://github.com/harrisonju1" target="_blank">Harrison Ju</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    );
+      );
   }
 });
 
-module.exports = Nav;
+module.exports= Nav;
