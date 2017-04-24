1e6f08220831bfe72d570f4bb027663f548c097a
harrisonju1 <harrisonju@gmail.com>
2017-04-23 23:07:21 -0400

    modify countdownform component and tests

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,8 +5,8 @@ var CountdownForm = React.createClass({
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
 
-    if (strSeconds.match(/^[0-9]*/)){ //.match expects a regex ^start must match 0-9 and *allows for many repetitions of 0-9
-      this.refs.seconds.value='';
+    if (strSeconds.match(/^[0-9]*$/)){ //.match expects a regex ^start must match 0-9 and *allows for many repetitions of 0-9
+      this.refs.seconds.value= '';
       this.props.onSetCountdown(parseInt(strSeconds, 10));
     }
   },
