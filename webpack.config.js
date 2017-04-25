a58fb2e5f006a917f263ae685f8ad9f6367516bc
harrisonju1 <harrisonju@gmail.com>
2017-04-24 22:02:25 -0400

    Added timer component and timer form to make timer functional

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,9 @@ module.exports = {
       Countdown:'app/components/Countdown.jsx',
       Clock:'app/components/Clock.jsx',
       CountdownForm:'app/components/CountdownForm.jsx',
-      Controls: 'app/components/Controls.jsx'
+      Controls: 'app/components/Controls.jsx',
+      TimerForm: 'app/components/TimerForm.jsx',
+      TimerControls: 'app/components/TimerControls.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
